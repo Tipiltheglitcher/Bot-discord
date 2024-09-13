50897a02650dbb002ab770f2f15bb6e45203c846
Tipiltheglitcher <144934540+Tipiltheglitcher@users.noreply.github.com>
2024-09-13 08:19:08 +0200

    Update serveur.js

--- serveur.js
+++ serveur.js
@@ -1,12 +1,8 @@
-import express from "express";
-const express = require('express')
+const express = require("express");
 const app = express()
-const port = process.env.PORT || 4000;
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(web_message)
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(3000, () => {
+  console.log(bot_loading_message.cyan)
 })
